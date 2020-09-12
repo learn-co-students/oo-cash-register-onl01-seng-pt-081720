16947cdf65b14ec88dd414ed2b06fa6bc877dd67
Hector_Jimenez <hjmontemayor@gmail.com>
2020-09-12 17:00:29 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,21 +7,14 @@ class CashRegister
         @discount = discount
         @total = 0
         @items_array = []
-        @items_history = Array.new
+        @last_item = Array.new
         @temp = []
     end
 
-    def add_item(title, price, quantity = 0)
-        if quantity > 0
+    def add_item(title, price, quantity = 1)
             quantity.times {@items_array << title} 
-            @items_history << [title, price, quantity]
-            @total += (price * quantity)
-        else
-            @items_array << title
-            @items_history << [title, price]
-            @total += price
-        end
-        
+            @last_item = [title, price, quantity]
+            @total += (price * quantity) 
     end
 
     def apply_discount
@@ -36,16 +29,10 @@ class CashRegister
     def items
         @items_array
     end
+ 
 
     def void_last_transaction
-        @temp = @items_history.pop
-        if @temp.length() > 2
-            @total = (@total - (@temp[1].to_i)*(@temp[2].to_i))
-            @total
-        else
-            @total = (@total - @temp[1].to_i)
-            @total
-        end
+            @total = (@total - ((@last_item[1].to_f)*(@last_item[2].to_f)))
     end
 
 end
